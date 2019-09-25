79c897f861633a999e8d87eb3c5c82aec1776b5a
Amir Hossein 00 <amirsorouri26@gmail.com>
2019-09-25 15:53:06 +0330

    session 1 added.

---
+++ session1/computeP/c_version/p.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+#define n   30000000000
+
+int main(){
+    double x, y, z;
+    int m = 0;
+    srand(time(NULL));
+    for (int i = 0; i < n; i++){
+        x = rand() / (double) RAND_MAX;
+        y = rand() / (double) RAND_MAX;
+        z = pow(x,2) + pow(y,2);
+        if(z < 1) m++;
+    }
+    printf("m = %d\n", m);
+    printf("final Value is: %f\n", 4*(double)m/(double)n);
+    return 0;
+}
